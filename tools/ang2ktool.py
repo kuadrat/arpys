c03021da4a8eb06b4d48cd9c66256caac9df80b5
Kevin Kramer <kevinpkramer@gmail.com>
2018-02-07 18:15:06 +0100

    Tools dir with ... tools

---
+++ tools/ang2ktool.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import laplace
+
+import dataloaders as dl
+import postprocessing as pp
+
+# Set up parser
+# ==============================================================================
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('filename', type=str,
+                   help='Name of the data file of which to apply ang2k \
+                    conversion')
+
+parser.add_argument('-s', '--shift', type=float, default=0,
+                   help='How much to shift the angular scale (in its units).')
+
+parser.add_argument('-l', '--lattice_constant', type=float, default=1,
+                   help='Lattice constant in Angstrom.')
+
+parser.add_argument('-v', '--vmax', type=float, default=1,
+                   help='Colorbar scaling percentage (0-1).')
+
+parser.add_argument('-c', '--cmap', type=str, default='plasma_r',
+                   help='Name of matplotlib colormap.')
+
+args = parser.parse_args()
+
+# Load and process data
+# ==============================================================================
+datadict = dl.load_data(args.filename)
+
+data = datadict['data']
+xscale = datadict['xscale']
+yscale = datadict['yscale']
+angles = datadict['angles']
+theta = datadict['theta']
+phi = datadict['phi']
+hv = datadict['hv']
+E_b = datadict['E_b']
+
+shape = data.shape
+# Quick fix for PSI "scans"
+if shape[0] != 1 :
+    print("Applying quick fix")
+    data = data[0]
+    data = data.reshape([1, shape[1], shape[2]])
+
+#for key, val in datadict.items() :
+#    if key in ['theta', 'phi', 'hv', 'E_b'] :
+#        print('{} {}'.format(key, val))
+        
+kx, ky = pp.angle_to_k(angles, theta, phi, hv, E_b, 
+                       lattice_constant=args.lattice_constant, 
+                       shift=args.shift, degrees=True)
+
+# Plotting
+# ==============================================================================
+fig, (ax0, ax1, ax2) = plt.subplots(3)
+
+vmax = args.vmax*data[0].max() 
+cmap = args.cmap
+
+ax0.plot(angles+args.shift, kx)
+ax1.pcolormesh(xscale, yscale, data[0], vmin=0, vmax=vmax, cmap=cmap)
+ax2.pcolormesh(kx, yscale, data[0], vmin=0, vmax=vmax, cmap=cmap)
+
+plt.show()
---
+++ tools/slice_map.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+description="""
+Script that takes ARPES map data (3D) and slices it ath a specified index 
+along a specified dimension to create cuts through the map and writes them to 
+files.
+"""
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from kustom.arpys import dataloaders as dl
+from kustom.arpys import postprocessing as pp
+from kustom import plotting as kplot
+
+# Set up parser
+# ==============================================================================
+
+parser = argparse.ArgumentParser(description=description)
+
+parser.add_argument('filename', type=str,
+                    help='Name of the map to take a slice from')
+
+args = parser.parse_args()
+
+# Parameters
+# ==============================================================================
+filename = args.filename
+filename = '/home/kevin/Documents/qmap/materials/Bi2201/2017_12_ALS/20171215_00438.fits'
+
+outfile = '20171215_00438_slice_x290.p'
+
+z = 415
+integrate = 10
+
+slice_dim = 2
+slice_ind = 60
+slice_dim = 1
+slice_ind = 290
+
+figsize = (4, 8)
+
+# Load the data
+dataloader = dl.Dataloader_ALS()
+datadict = dataloader.load_data(filename)
+print(datadict['xscale'])
+data = datadict['data']
+# Extract a Fermi surface map
+fsm = pp.make_slice(data, d=0, i=z, integrate=integrate)
+
+# Create a cut at the specified index
+cut = pp.make_slice(data, d=slice_dim, i=slice_ind, integrate=1)
+
+# Prepare plotting
+fig = plt.figure(figsize=figsize, projection='cursor')
+
+# Plot the map and indicate the location of the cut
+ax1.pcolormesh(fsm)
+fsm_max = fsm.shape[ slice_dim%2 ]
+x = [0, fsm_max]
+y = 2*[slice_ind]
+if slice_dim != 1 :
+    # Swap
+    x, y = y, x
+
+ax1.plot(x, y, 'r-', lw=1)
+
+# Plot the cut
+ax2.pcolormesh(cut)
+
+import pickle
+
+# Reshape data before outputting
+x, y = cut.shape
+new_data = cut.reshape([1, x, y])
+datadict.update({'data': new_data})
+print(datadict)
+
+# Write an output file (binary)
+with open(outfile, 'wb') as f :
+    pickle.dump(datadict, f)
+
+plt.show()
